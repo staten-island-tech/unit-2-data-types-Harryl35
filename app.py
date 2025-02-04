2303bd88167768a6e9839e45b8395b3dbedad57a
Harryl35 <HArryl29@nycstudents.net>
2025-02-04 09:25:10 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+# Strings represent characters, names, words    
+name = "Kammy"
+#intergers for WHOLE numbers    
+age = 14
+#boolean is True or False used for evalutations 
+graduated = False
+#Floats for decimal 
+money = 4.50
+
+def add(x,y):
+    print(round(x + y))
+# add(54,"11")
+#input seeks user response and outputs that into the variable 
+#bill value will be equal to the response of the user
+bill = float(input("How much was the bill?"))
+print(bill)
+add(15,bill)
+
+""" #Lists which store lists of data
+students = ["kammy", "Rachel", "Denis", "Ian"]
+#similar for i in range(4)
+money = [1,2,3,4,5,6]
+#scalable
+for students in students:
+    print(students) """
+x = 0
+for money in moneys 
+    x = x + money
+    print(x)
